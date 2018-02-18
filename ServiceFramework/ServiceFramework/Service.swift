d3efdae34ff4af00c18570a577b806de8d0f2dd9
hemavenkat <hemavenkat59@gmail.com>
2018-02-18 21:17:49 +0530

    Finished Framework

---
+++ ServiceFramework/ServiceFramework/Service.swift
@@ -0,0 +1,19 @@
+//
+//  Service.swift
+//  ServiceFramework
+//
+//  Created by HEMA VENKATARAMAN on 18/02/2018.
+//  Copyright © 2018 HEMA VENKATARAMAN. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() {
+        
+    }
+    public static func doSomething() -> String {
+        return "Created a Framework! Well Done!!"
+    }
+}
---
+++ ServiceFramework/ServiceFramework/ServiceFramework.h
@@ -0,0 +1,19 @@
+//
+//  ServiceFramework.h
+//  ServiceFramework
+//
+//  Created by HEMA VENKATARAMAN on 18/02/2018.
+//  Copyright © 2018 HEMA VENKATARAMAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ServiceFramework.
+FOUNDATION_EXPORT double ServiceFrameworkVersionNumber;
+
+//! Project version string for ServiceFramework.
+FOUNDATION_EXPORT const unsigned char ServiceFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ServiceFramework/PublicHeader.h>
+
+
